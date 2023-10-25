f8a45d29601e2ec45580fd0e9b3743be908e13be
Rajib Kumar Halder <rajib.cse1346@gmail.com>
2023-10-25 14:24:27 +0600

    Update W-kNN.py

--- W-kNN.py
+++ W-kNN.py
@@ -1,68 +1,54 @@
-# Python3 program to implement the 
-# weighted K nearest neighbour algorithm.  
-  
-import math  
-  
-def weightedkNN(points,p,k=3):  
-    '''  
-    This function finds classification of p using  
-    weighted k nearest neighbour algorithm. It assumes only two  
-    two classes and returns 0 if p belongs to class 0, else  
-    1 (belongs to class 1).  
-  
-    Parameters -  
-        points : Dictionary of training points having two keys - 0 and 1  
-            Each key have a list of training data points belong to that  
-  
-        p : A tuple ,test data point of form (x,y)  
-  
-        k : number of nearest neighbour to consider, default is 3  
-    '''
-  
-    distance=[]  
-    for group in points:  
-        for feature in points[group]:  
-  
-            #calculate the euclidean distance of p from training points  
-            euclidean_distance = math.sqrt((feature[0]-p[0])**2 +(feature[1]-p[1])**2)  
-  
-            # Add a tuple of form (distance,group) in the distance list  
-            distance.append((euclidean_distance,group))  
-  
-    # sort the distance list in ascending order  
-    # and select first k distances  
-    distance = sorted(distance)[:k]  
-  
-    freq1 = 0 # weighted sum of group 0  
-    freq2 = 0 # weighted sum of group 1  
-  
-    for d in distance: 
-        if d[1] == 0: 
-            freq1 += (1 / d[0]) 
-              
-        elif d[1] == 1:  
-            freq2 += (1 /d[0]) 
-              
-  
-    return 0 if freq1>freq2 else 1
-  
-# Driver function  
-def main():  
-  
-    # Dictionary of training points having two keys - 0 and 1  
-    # key 0 have points belong to class 0  
-    # key 1 have points belong to class 1  
-  
-    points = {0:[(0, 4),(1, 4.9),(1.6, 5.4),(2.2, 6),(2.8, 7),(3.2, 8),(3.4, 9)],  
-            1:[(1.8, 1),(2.2, 3),(3, 4),(4, 4.5),(5, 5),(6, 5.5)]}  
-  
-    # query point p(x,y)  
-    p = (2, 4)  
-  
-    # Number of neighbours  
-    k = 5
-  
-    print("The value classified to query point is: {}".format(weightedkNN(points,p,k)))  
-  
-if __name__ == '__main__':  
-    main()  
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from collections import Counter
+
+# Step 1: Load the dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ["sepal-length", "sepal-width", "petal-length", "petal-width", "class"]
+dataset = pd.read_csv(url, names=names)
+
+# Step 2: Preprocess the data
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Step 3: Implement weighted k-NN
+def euclidean_distance(x1, x2):
+    return np.sqrt(np.sum((x1 - x2) ** 2))
+
+class WeightedKNN:
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X):
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
+
+    def _predict(self, x):
+        distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+        k_indices = np.argsort(distances)[:self.k]
+        k_distances = [distances[i] for i in k_indices]
+        k_labels = [self.y_train[i] for i in k_indices]
+
+        # Calculate the weighted votes
+        label_weights = {label: 0 for label in set(k_labels)}
+        for idx, label in enumerate(k_labels):
+            weight = 1 / (k_distances[idx] ** 2 + 1e-5)  # Avoid division by zero
+            label_weights[label] += weight
+
+        # Return the label with the highest weight
+        return max(label_weights, key=label_weights.get)
+
+# Create k-NN classifier and train it
+clf = WeightedKNN(k=3)
+clf.fit(X_train, y_train)
+predictions = clf.predict(X_test)
+
+# Step 4: Evaluate the model
+accuracy = np.mean(predictions == y_test)
+print(f"Accuracy: {accuracy * 100:.2f}%")
